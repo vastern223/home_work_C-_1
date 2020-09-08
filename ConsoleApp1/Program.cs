5fabc117446faa210327fd4e570874a26b0c2091
Василь Колотюк <vasakolotuk02@gmail.com>
2020-09-08 15:52:58 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //--------------------------------1 завдання
+
+            //String word;
+            //int num = 0;
+            //do
+            //{
+            //    Console.Write("Enter the symbol:");
+            //    word = Console.ReadLine();
+            //    if (word == " ")
+            //    {
+            //        num++;
+            //    }
+
+            //} while (word != ".");
+
+            //Console.WriteLine("number of spaces:" + num);
+
+
+
+            //------------------------------------2 завдання
+
+            //Console.WriteLine("Enter the ticket number");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 999999 && number < 100000)
+            //{
+            //    Console.WriteLine("Error");
+            //}
+            //else
+            //{
+            //    int count1 = number % 10;
+            //    int count2 = (number % 100) / 10;
+            //    int count3 = (number % 1000) / 100;
+            //    int count4 = (number % 10000) / 1000;
+            //    int count5 = (number % 100000) / 10000;
+            //    int count6 = (number % 1000000) / 100000;
+            //    if ((count1 + count2 + count3) == (count4 + count5 + count6))
+            //    {
+            //        Console.WriteLine("this ticket is lucki");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("this ticket is not lucki");
+            //    }
+            //}
+
+
+            //-----------------------------3 завдання
+
+            //int A = 3;
+            //int B = 7;
+
+            //for (int i = A; i <= B; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+
+            //----------------------------4 завдання
+
+            //Console.WriteLine("enter number:");
+            //int number1 = int.Parse(Console.ReadLine());
+            //string number2 = number1.ToString();
+            //char[] inputarray = number2.ToCharArray();
+            //Array.Reverse(inputarray);
+            //number2 = new string(inputarray);
+            //Console.WriteLine("number:" + number2);
+
+        }
+    }
+}
